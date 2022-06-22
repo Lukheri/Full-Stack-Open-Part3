fc3d71216ddd6cdb8cac3ff8fa14aa906091a90c
Lukheri <luke.contrivida@gmail.com>
2022-06-22 18:13:16 +0800

    added static middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(cors())
 
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 // app.use(morgan('tiny'))
